639b5e83dd5e8d9e4046c97a7b9a7958ecea9d16
Hirofumi Katayama <katayama.hirofumi.mz@gmail.com>
2015-06-17 00:18:11 +0900

    fifty seventh commit

--- examples/make_switch_virtual/v_file.c
+++ examples/make_switch_virtual/v_file.c
@@ -581,6 +581,7 @@ int v_fsave(v_LPCSTR fname, v_LPFILE v_fp, v_LPCSTR modes)
         {
             if (s_ofsOld == pofs)
             {
+                /* TODO: FIXME if you can */
                 fp = NULL;
                 CopyMemory(&fp, &pofs->Reserved1, sizeof(DWORD));
                 if (fp == NULL)
@@ -604,7 +605,7 @@ int v_fsave(v_LPCSTR fname, v_LPFILE v_fp, v_LPCSTR modes)
 
         if (style & OF_VERIFY)
         {
-            /* TODO & FIXME */
+            /* TODO: FIXME if you can */
         }
 
         fp = NULL;
